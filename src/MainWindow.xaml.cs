065553899e616683736c5ed9acceade9341e310e
Baiyi "Patrick" Liao <lbypatrick@gmail.com>
2018-05-19 02:01:30 -0400

    Visual Improvements
    Lowered transparency + Smaller window + less window keep time + New Icon

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace key_assist {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        public const int SCAN_INTERVAL = 15;
+        public const int WINDOW_KEEP_TIME = 1500;
+        public const double TRANSPARENCY = 0.7;
+        public const int NUM_TILES = 2;
+        private int _window_alive_time = 0;
+
+        private int _window_width;
+        private int _window_height;
+        private int _tile_length;
+        private int _tile_margin;
+
+        private bool _window_visible;
+
+        public void ShowWindowThenHide() {
+
+            //Just make it show longer...
+            if (_window_visible) {
+                if (_window_alive_time < 1300) {
+                    _window_alive_time = WINDOW_KEEP_TIME;
+                }
+                return;
+            }
+
+            else {
+                new Thread(delegate () {
+
+                    ShowWindow();
+                    _window_alive_time = WINDOW_KEEP_TIME;
+
+                    while (_window_alive_time > 0) {
+
+                        Thread.Sleep(200);
+                        _window_alive_time -= 200;
+                    }
+                    _window_alive_time = 0;
+
+                    HideWindow();
+                }).Start();
+            }
+        }
+        public void
+        InitWindow() {
+
+            //Set appropriate window size
+            _window_height = (int)(SystemParameters.VirtualScreenHeight / 7);
+            _window_width = (int)(SystemParameters.VirtualScreenWidth * NUM_TILES * 0.1);
+            this.Top = SystemParameters.VirtualScreenHeight / 2 + 200;
+            this.Left = SystemParameters.VirtualScreenWidth / 2 - _window_width / 2;
+            this.Height = _window_height;
+            this.Width = _window_width;
+
+            //Set Window Background color
+            this.Background.Opacity = TRANSPARENCY;
+
+            //Set Startup Location and window state
+            this.WindowStartupLocation = WindowStartupLocation.Manual;
+            this.WindowState = WindowState.Normal;
+
+            //Now hide the window
+            this.ShowActivated = false;
+            this.Opacity = 0;
+            _window_visible = false;
+
+        }
+
+        public void
+        InitIcons() {
+
+            _tile_length = _window_height * 3 / 5;
+            _tile_margin = _window_height / 10;
+            
+
+
+            //Load Source
+            caps_tile.Source = Util.getImage("Icons/caps_lock.png");
+            num_tile.Source = Util.getImage("Icons/num_lock.png");
+
+            caps_tile.Height =  _tile_length;
+            caps_tile.Width  =  _tile_length;
+            num_tile.Height = _tile_length;
+            num_tile.Height = _tile_length;
+
+            //Enable Anti-aliasing
+            Util.EnableAA(caps_tile);
+            Util.EnableAA(num_tile);
+            
+            //Put to center
+            Util.PlaceElement(caps_tile, 0, 0, 20);
+            Util.PlaceElement(num_tile, 0, 0, 20);
+            
+        }
+
+        public
+        MainWindow() {
+                InitializeComponent();
+                InitWindow();
+                InitIcons();
+                ListenKeys();
+        }
+
+        public void
+        ListenKeys() {
+
+            new Thread(delegate () {
+                while (true) {
+
+                    KeyUtil.update();
+
+                    if (KeyUtil.IsKeyboardChanged()) {
+
+                        if (KeyUtil.IsKeyChanged(KeyUtil.CAPS_KEY)) {
+                            Util.RunLater((Action)delegate () {
+                                caps_tile.Opacity = KeyUtil.IsKeyToggled(KeyUtil.CAPS_KEY) ? 1 : 0.5;
+                            });
+                        }
+
+                        if (KeyUtil.IsKeyChanged(KeyUtil.NUM_KEY)) {
+                            Util.RunLater((Action)delegate () {
+                                num_tile.Opacity = KeyUtil.IsKeyToggled(KeyUtil.NUM_KEY) ? 1 : 0.5;
+                            });
+                        }
+
+                        ShowWindowThenHide();
+                    }
+                    Thread.Sleep(SCAN_INTERVAL);
+                }
+            }).Start();
+        }
+
+        private void ShowWindow() {
+            Util.RunLater(delegate(){
+                this.Opacity = 1;
+                this.ShowActivated = true;
+                _window_visible = true;
+            });
+        }
+
+        private void HideWindow() {
+            Util.RunLater(delegate () {
+                this.ShowActivated = false;
+                this.Opacity = 0;
+            });
+            _window_visible = false;
+        }
+    }
+}
---
+++ src/Util.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace key_assist {
+    class Util {
+        public static void 
+        RunLater(Action action) {
+            Application.Current.Dispatcher.BeginInvoke(action);
+        }
+
+        public static BitmapImage
+        getImage(String path) {
+            return new BitmapImage(new Uri(path, UriKind.Relative));
+        }
+
+        public static void PlaceElement(FrameworkElement element, int x, int y) {
+            PlaceElement(element, x, y, 0);
+        }
+
+        public static void EnableAA(DependencyObject obj) {
+            RenderOptions.SetBitmapScalingMode(obj, BitmapScalingMode.HighQuality);
+        }
+
+        public static void PlaceElement(FrameworkElement element, int x, int y, int extra_thinkness) {
+
+            bool is_upward = y > 0,
+                 is_leftward = x < 0;
+
+            element.HorizontalAlignment = HorizontalAlignment.Center;
+            element.VerticalAlignment = VerticalAlignment.Center;
+
+            element.Margin = new Thickness {
+                Left = (!(is_leftward) ? Math.Abs(x) : 0) + extra_thinkness,
+                Right = (is_leftward ? Math.Abs(x) : 0) + extra_thinkness,
+                Top = (!(is_upward) ? Math.Abs(y) : 0) + extra_thinkness,
+                Bottom = (is_upward ? Math.Abs(y) : 0) + extra_thinkness
+            };
+        }
+    }
+}
